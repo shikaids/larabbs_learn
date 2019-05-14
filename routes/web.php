1b3ff8984bcfe6c9a41de1bff0f1f919bc4df42c
shikaids <nelson.leong@qq.com>
2019-05-14 09:00:01 +0000

    生成用户认证代码

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,31 @@
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+
+/**
+ * 为了更直观注释Auth::routes()，使用下面路由代替。实现功能是一样的。
+ */
+//Auth::routes();
+
+// 用户登录身份验证相关路由
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// 游客注册相关路由
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// 密码重置相关路由
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+// 邮件认证证相关路由
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+// 本系统已有首页，这是脚手架默认生成的，删掉
+//Route::get('/home', 'HomeController@index')->name('home');
