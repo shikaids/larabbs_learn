899a107c44a182a51a528c33d5eeda3f93f46e90
shikaids <nelson.leong@qq.com>
2019-05-22 15:14:12 +0000

    上传头像与显示头像

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,11 +13,14 @@ class User extends Authenticatable implements MustVerifyEmailContract
 
     /**
      * The attributes that are mass assignable.
+     * 数据白名单，为了安装，数据库的数据表的记录是不允许修改更新的。
+     * $fillable是字段可修改更新的白名单，添加字段进行$fillable数组，
+     * 则字段可被修改更新
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'introduction',
+        'name', 'email', 'password', 'introduction', 'avatar',
     ];
 
     /**
