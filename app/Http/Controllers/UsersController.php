e35d45cb31bdfcd79386a089c802f50b9265acd2
shikaids <nelson.leong@qq.com>
2019-05-23 04:36:19 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,18 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
-    //
+    public function __construct()
+    {
+        // 除了show可以被游客访问，edit、update只能被登录用户访问
+        // 第一个为中间件的名称；
+        // 第二个为要进行过滤的动作；
+        // 我们通过 except 方法来设定 指定动作 不使用 Auth 中间件进行过滤，
+        // 意为 —— 除了此处指定的动作以外，所有其他动作都必须登录用户才能访问，类似于黑名单的过滤机制。
+        // 相反的还有 only 白名单方法，将只过滤指定动作。
+        // 我们提倡在控制器 Auth 中间件使用中，首选 except 方法，这样的话，当你新增一个控制器方法时，默认是安全的，此为最佳实践。
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -18,11 +29,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         //$user->update($request->all());
         //赋值 $data 变量，以便对更新数据的操作
         $data = $request->all();
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -9,6 +9,8 @@ class AuthServiceProvider extends ServiceProvider
 {
     /**
      * The policy mappings for the application.
+     * 手动指定策略与模型关联。
+     * 具体写法可参看weibo例子
      *
      * @var array
      */
@@ -18,6 +20,7 @@ class AuthServiceProvider extends ServiceProvider
 
     /**
      * Register any authentication / authorization services.
+     * 自动授权注册写在boot方法内
      *
      * @return void
      */
@@ -25,6 +28,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // 修改策略自动发现的逻辑
+        Gate::guessPolicyNamesUsing(function ($modelClass) {
+            // 动态返回模型对应的策略名称，如：// 'App\Model\User' => 'App\Policies\UserPolicy',
+            return 'App\Policies\\' . class_basename($modelClass) . 'Policy';
+        });
     }
 }
